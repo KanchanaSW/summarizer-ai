2635c33be05884af288d41d4257e04c39a960a6a
Kanchana Walagambahu <kanchana.walagambahu@axiatadigitallabs.com>
2023-08-20 14:08:10 +0530

    store cnfigured

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
 
-export default Demo
+  const handelSubmit = async () => {
+
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handelSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter Url"
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            value={article.url}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-grey-700 peer-focus:text-grey-700"
+          >
+            🍳
+          </button>
+        </form>
+
+        {/* browse URL history */}
+      </div>
+      {/* display results */}
+    </section>
+  );
+};
+
+export default Demo;
